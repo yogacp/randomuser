de697142e2422c8bc385cf92ba51ef03594d4ce2
yogacp <ichi.emo@gmail.com>
2018-05-09 06:30:36 +0700

    initial commit

---
+++ app/src/main/java/app/randomuser/tabsquare/RandomUserApp.kt
@@ -0,0 +1,12 @@
+package app.randomuser.tabsquare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomUserApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
